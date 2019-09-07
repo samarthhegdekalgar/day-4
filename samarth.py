218b7cebeb8437a1e61ec4513592116a13397a76
Samartha Hegde <samarthhegdekalgar@gmail.com>
2019-09-07 15:17:47 +0530

    completed assignments

--- samarth.py
+++ samarth.py
@@ -1,1 +1,23 @@
 print("Hello World")
+#print (2+-2
+
+print(21//2.0)
+a=10
+b='Sam'
+print(a==b)
+
+second=42
+minute=42
+
+print("Number of seconds:",minute*60+second)
+
+miles=10
+km=1.61
+print("Number of KM:",km*miles)
+
+distance=10
+time_in_seconds=42*60+42
+time_in_mint=time_in_seconds/60
+average_time=distance/time_in_mint
+average_time_in_miles=average_time*1.61
+print("Average speed:",float(average_time_in_miles),"miles")
